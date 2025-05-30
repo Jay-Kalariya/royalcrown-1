34f52b9347c31b92513e90dfebd2fa510fd0ab22
Jay-Kalariya <Jay-kalariya>
2025-05-30 14:51:56 +0530

    m --j

--- src/components/aboutUs_productsOne/page.jsx
+++ src/components/aboutUs_productsOne/page.jsx
@@ -92,7 +92,9 @@ const Page = () => {
     if (selectedTag !== "all") params.set("tag", selectedTag);
     if (pageNumber !== 1) params.set("page", pageNumber);
 
-    router.push(`?${params.toString()}`, { scroll: false });
+    router.push(`?${params.toString()}`,
+    //  { scroll: false }
+  );
   };
   useEffect(() => {
     updateQueryParams();
